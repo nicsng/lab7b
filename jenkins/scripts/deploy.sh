32ebd11de6544b1bd5c8eb56b96df5a4d6901fdb
Nicholas Sng <nicholas_sng@ymail.com>
2024-07-20 16:21:33 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
  
 set -x
-docker run -d -p 10000:10000 --name my-apache-php-app -v c:\\Users\\nicholassng\\Downloads\\lab7b\\src:/var/www/html php:7.2-apache
+docker run -d -p 10000:10000 --name my-apache-php-app -v c:/Users/nicholassng/Downloads/lab7b/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
